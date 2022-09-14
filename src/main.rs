60bdfb4286c058c16f625fe316e40d8d51acb40d
Brian Ramirez <ramirez7358@gmail.com>
2022-09-14 15:28:16 -0300

    Add some operators, identifier and keywords

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod lexer;
+
+use lexer::tokenize_all_collect;
+
 fn main() {
-    println!("Hello, world!");
+    let test = "object Singleton {}";
+
+    let toks = tokenize_all_collect(test);
+
+    println!("#toks = {:?}", toks);
 }
