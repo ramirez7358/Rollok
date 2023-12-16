74536f85ee4528384d7faffecd21911b7eb35e2e
Brian Ramirez <ramirez7358@gmail.com>
2023-12-16 18:14:11 -0300

    Create a tree structure with only var statement

--- src/ast.rs
+++ src/ast.rs
@@ -1,40 +1,42 @@
+use crate::token::Token;
+
 trait Node {
     fn token_literal(&self) -> String;
     fn print_string(&self) -> String;
 }
 
 enum StatementNode {
-    Let,
+    Var(VarStatement),
 }
 
 impl Node for StatementNode {
     fn token_literal(&self) -> String {
         match self {
-            Self::Let => todo!()
+            Self::Var(var_stmt) => var_stmt.token_literal()
         }
     }
 
     fn print_string(&self) -> String {
         match self {
-            Self::Let => todo!()
+            Self::Var(var_stmt) => var_stmt.print_string()
         }
     }
 }
 
 enum ExpressionNode {
-    Identifier,
+    IdentifierNode(Identifier),
 }
 
 impl Node for ExpressionNode {
     fn token_literal(&self) -> String {
         match self {
-            Self::Identifier => todo!()
+            Self::IdentifierNode(identifier) => identifier.token_literal()
         }
     }
 
     fn print_string(&self) -> String {
         match self {
-            Self::Identifier => todo!()
+            Self::IdentifierNode(identifier) => identifier.print_string()
         }
     }
 }
@@ -47,7 +49,7 @@ impl Node for Program {
     fn token_literal(&self) -> String {
         if self.statements.len() > 0 {
             match &self.statements[0] {
-                StatementNode::Let => todo!()
+                StatementNode::Var(var_stmt) => var_stmt.token_literal()
             }
         } else {
             String::from("")
@@ -61,5 +63,48 @@ impl Node for Program {
             output.push_str(stmt.print_string().as_str());
         }
         output
+    }
+}
+
+struct VarStatement {
+    token: Token,
+    name: Identifier,
+    value: Option<ExpressionNode>
+}
+
+impl Node for VarStatement {
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
+
+    fn print_string(&self) -> String {
+        let mut out = String::from("");
+
+        out.push_str(&self.token_literal());
+        out.push_str(" ");
+        out.push_str(&self.name.print_string());
+        out.push_str(" = ");
+
+        if let Some(value) = &self.value {
+            out.push_str(&value.print_string());
+        };
+        out.push_str(";");
+
+        out
+    }
+}
+
+struct Identifier {
+    token: Token,
+    value: String
+}
+
+impl Node for Identifier {
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
+
+    fn print_string(&self) -> String {
+        self.value.clone()
     }
 }
