510071614c803d92b11f41062c74514ba73a0ad1
Brian Ramirez <ramirez7358@gmail.com>
2023-12-27 19:31:09 -0300

    Add test to parser

--- src/parser.rs
+++ src/parser.rs
@@ -9,7 +9,7 @@ struct Parser {
 }
 
 impl Parser {
-    fn new(lexer: Lexer) -> Self {
+    pub fn new(lexer: Lexer) -> Self {
         let mut parser = Self {
             lexer,
             current_token: Default::default(),
@@ -28,5 +28,69 @@ impl Parser {
 
     fn parse_program(&mut self) -> Option<Program> {
         None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::ast::{Node, StatementNode};
+    use crate::lexer2::Lexer;
+    use crate::parser::Parser;
+
+    #[test]
+    fn test_var_statements() {
+        let input = r#"
+            var x = 4;
+            var y = 10;
+            var foobar = 83838;
+        "#;
+
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+        let program = parser.parse_program();
+
+        match program {
+            Some(program) => {
+                assert_eq!(
+                    program.statements.len(),
+                    3,
+                    "statements does not contain 3 statements. got = {}",
+                    program.statements.len()
+                );
+
+                let expected = vec!["x", "y", "foobar"];
+
+                for (idx, exp) in expected.into_iter().enumerate() {
+                    let stmt = &program.statements[idx];
+                    test_var_statement(stmt, exp);
+                }
+            }
+            None => panic!("parse program should not be none"),
+        }
+    }
+
+    fn test_var_statement(stmt: &StatementNode, expected: &str) {
+        assert_eq!(
+            stmt.token_literal(),
+            "var",
+            "token literal not `var`. got={}",
+            stmt.token_literal()
+        );
+
+        match stmt {
+            StatementNode::Var(var_stmt) => {
+                assert_eq!(
+                    var_stmt.name.value, expected,
+                    "VarStatement name value not {}. got={}",
+                    expected, var_stmt.name.value
+                );
+                assert_eq!(
+                    var_stmt.name.token_literal(), expected,
+                    "VarStatement name value not {}. got={}",
+                    expected, var_stmt.name.token_literal()
+                );
+            }
+            other => panic!("stmt is not VarStatement. got={:?}", other),
+        }
     }
 }
